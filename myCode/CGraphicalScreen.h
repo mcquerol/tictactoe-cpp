e91113d2f254856e84384fa228324c0c194cdf5c
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2021-12-06 21:09:14 +0000

    added myCode\CGraphicalScreen.h

---
+++ myCode/CGraphicalScreen.h
@@ -0,0 +1,38 @@
+/***************************************************************************
+*============= Copyright by Darmstadt University of Applied Sciences =======
+****************************************************************************
+* Filename        : CGRAPHICALSCREEN.H
+* Author          :
+* Description     :
+*
+*
+****************************************************************************/
+
+#ifndef CGRAPHICALSCREEN_H
+#define CGRAPHICALSCREEN_H
+class CGraphicalScreen {
+public:
+
+	/** CGraphicalScreen constructor
+	 *
+	 * EMPTY constructor (for future implementation)
+	 */
+	CGraphicalScreen();
+
+	/** CGraphicalScreen destructor
+	 *
+	 * EMPTY destructor (for future implementation)
+	 */
+	~CGraphicalScreen();
+
+	/** CGraphicalScreen::printGUI
+	 *
+	 * EMPTY print function (output a tic tac toe gui in the future)
+	 */
+	void printGUI();
+
+};
+/********************
+**  CLASS END
+*********************/
+#endif /* CGRAPHICALSCREEN_H */

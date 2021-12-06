5fd54f7958ba9b78f414096641440ef333448b25
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2021-12-06 21:07:30 +0000

    added myCode\CGraphicalScreen.cpp

---
+++ myCode/CGraphicalScreen.cpp
@@ -0,0 +1,25 @@
+#include "CGraphicalScreen.h"
+
+/** CGraphicalScreen constructor
+ *
+ * EMPTY constructor (for future implementation)
+ */
+CGraphicalScreen::CGraphicalScreen()
+{
+
+}
+
+/** CGraphicalScreen destructor
+ *
+ * EMPTY destructor (for future implementation)
+ */
+CGraphicalScreen::~CGraphicalScreen(){}
+
+/** CGraphicalScreen::printGUI
+ *
+ * EMPTY print function (output a tic tac toe gui in the future)
+ */
+void CGraphicalScreen::printGUI()
+{
+	//print GUI
+}
